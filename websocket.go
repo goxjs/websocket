dc8f3641b71c325fb3e62fc9168edcc3f5b55f1f
Dmitri Shuralyov <shurcooL@gmail.com>
2015-08-21 19:53:27 -0700

    Simplify code.
    This largely reverts the change of 937cf0b. Instead of explicitly
    implementing net.Conn methods, we can just rely on the return value
    being net.Conn (done in 1cb65a0), which guarantees a stable,
    predictable API for users for this package.

--- websocket.go
+++ websocket.go
@@ -4,24 +4,14 @@ package websocket
 
 import (
 	"net"
-	"time"
 
 	"golang.org/x/net/websocket"
 )
 
 // conn represents a WebSocket connection.
 type conn struct {
-	ws *websocket.Conn
+	*websocket.Conn
 }
-
-func (c conn) Read(b []byte) (n int, err error)   { return c.ws.Read(b) }
-func (c conn) Write(b []byte) (n int, err error)  { return c.ws.Write(b) }
-func (c conn) Close() error                       { return c.ws.Close() }
-func (c conn) LocalAddr() net.Addr                { return c.ws.LocalAddr() }
-func (c conn) RemoteAddr() net.Addr               { return c.ws.RemoteAddr() }
-func (c conn) SetDeadline(t time.Time) error      { return c.ws.SetDeadline(t) }
-func (c conn) SetReadDeadline(t time.Time) error  { return c.ws.SetReadDeadline(t) }
-func (c conn) SetWriteDeadline(t time.Time) error { return c.ws.SetWriteDeadline(t) }
 
 // Dial opens a new client connection to a WebSocket.
 //
@@ -38,5 +28,5 @@ func Dial(url, origin string) (net.Conn, error) {
 	// the Write method sends bytes as binary rather than text frames.
 	ws.PayloadType = websocket.BinaryFrame
 
-	return conn{ws: ws}, nil
+	return conn{Conn: ws}, nil
 }
--- websocket_js.go
+++ websocket_js.go
@@ -4,28 +4,18 @@ package websocket
 
 import (
 	"net"
-	"time"
 
 	"github.com/gopherjs/websocket"
 )
 
 type conn struct {
-	ws *websocket.Conn
+	*websocket.Conn
 }
-
-func (c conn) Read(b []byte) (n int, err error)   { return c.ws.Read(b) }
-func (c conn) Write(b []byte) (n int, err error)  { return c.ws.Write(b) }
-func (c conn) Close() error                       { return c.ws.Close() }
-func (c conn) LocalAddr() net.Addr                { return c.ws.LocalAddr() }
-func (c conn) RemoteAddr() net.Addr               { return c.ws.RemoteAddr() }
-func (c conn) SetDeadline(t time.Time) error      { return c.ws.SetDeadline(t) }
-func (c conn) SetReadDeadline(t time.Time) error  { return c.ws.SetReadDeadline(t) }
-func (c conn) SetWriteDeadline(t time.Time) error { return c.ws.SetWriteDeadline(t) }
 
 func Dial(url, _ string) (net.Conn, error) {
 	ws, err := websocket.Dial(url)
 	if err != nil {
 		return nil, err
 	}
-	return conn{ws: ws}, nil
+	return conn{Conn: ws}, nil
 }
